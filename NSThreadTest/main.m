bbc3853ff59975de958320a5c9c78d17f13db5fc
Michael Dales <mdales@mac.com>
2012-11-19 11:17:45 +0000

    Initial Commit

---
+++ NSThreadTest/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  NSThreadTest
+//
+//  Created by Michael Dales on 19/11/2012.
+//  Copyright (c) 2012 Michael Dales. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
